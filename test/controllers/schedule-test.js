77b50cec247b027a227fa3edce66f54ddb17a805
Greg Cobb <gregmcobb@gmail.com>
2012-08-16 16:50:43 -0400

    schedule tests

--- test/controllers/schedule-test.js
+++ test/controllers/schedule-test.js
@@ -1,30 +1,126 @@
-var Browser = require("zombie")
-  , should = require("should");
-browser = new Browser();
+var soda_sync = require('soda-sync')
+  , soda = soda_sync.soda
+  , SodaCan = soda_sync.SodaCan;
+
+var browser = null;
+
+SodaCan = SodaCan({
+  "with": function() {
+    return browser;
+  },
+  pre: function() {
+    return this.timeout(5000);
+  }
+});
+browser = soda.createClient({
+  host: "localhost",
+  port: 4444,
+  url: "http://courseshark.dev",
+  browser: "googlechrome",
+  mode: 'sync'
+});
+browser.on('command', function(cmd, args){
+  console.log(' \x1b[33m%s\x1b[0m: %s', cmd, args.join(', '));
+});
 
 describe('Schedule controller', function(){
 
-  describe('Schedule page', function(){
+  describe("Schedule page", function(){
+    var t;
 
-    it("should require you to select school if not logged in", function(done) {
-      browser.visit("http://courseshark.dev/schedule", function(){
-        browser.success.should.be.ok;
-        browser.location.pathname.should.equal('/choose-school');
-        browser.html("h1").should.include('Before we continue, we need to know what school you go to');
-        done();
-      });
-    });
+    it("/schedule requires school selection if not logged in", SodaCan(function(){
+      t = this;
+      t.session();
+      t.open('/schedule');
+      t.waitForTextPresent('Before we continue, we need to know what school you go to');
+    }));
 
-    // before(function(done){
-    //   browser.visit("http://courseshark.dev",function(){
-    //     browser.pressButton("Sign In");
-    //   });
-    // })
+    it("after school selection take you to the schedule page", SodaCan(function(){
+      t.click('schools_gatech');
+      t.waitForTextPresent('CRN List');
+      t.testComplete();
+    }));
 
-    it("should take you to the schedule if you are logged in", function(){
-      return true;
-    });
+    it("/schedule routes to schedule if you are logged in", SodaCan(function(){
+      t = this;
+      t.session();
+      t.open('/schedule');
+      t.waitForTextPresent('Before we continue, we need to know what school you go to');
+      t.click('signInButton');
+      t.waitForTextPresent('Login');
+      t.type('login_email','greg@greg.com');
+      t.type('login_password','greg');
+      t.click('loginButton');
+      t.waitForElementPresent('accountDropdown');
+      t.open('/schedule');
+      t.waitForTextPresent('CRN List');
+    }));
+
+    it ("select a subject and a class", SodaCan(function(){
+      t.waitForElementPresent('id=major-id')
+      t.select('id=major-id','index=1');
+      t.waitForTextPresent('2101');
+      t.select('id=course-id','index=1');
+      t.verifyElementPresent('id=class-list');
+    }));
+
+    it ("add a class to schedule", SodaCan(function(){
+      t.waitForElementPresent('xpath=//div[@class="section-option"]');
+      t.click('xpath=//div[@class="section-option"]');
+      t.verifyElementPresent('xpath=//div[@class="wk-event-listing"]');
+    }));
+
+    //Todo: Test mouseover of class list
+
+    it ("display details on demand", SodaCan(function(){
+      t.mouseOver('xpath=//div[contains(@class,"wk-event-listing")][1]');
+      t.verifyTextPresent('Section Number');
+      t.mouseOut('xpath=//div[contains(@class,"wk-event-listing")][1]');
+    }));
+
+    it ("save a schedule", SodaCan(function(){
+      t.click('id=save-button');
+      t.waitForElementPresent('save_name');
+      t.click('save_name')
+      t.focus('save_name')
+      t.type('save_name','awesome')
+      t.typeKeys('save_name','awesome');
+      t.click('save_save');
+      t.verifyNotVisible('id=name-input');
+    }));
+
+    it ("create a new schedule", SodaCan(function(){
+      t.click('id=new-button');
+      t.waitForElementPresent('id=term-selector');
+      t.click('id=new-term-submit');
+      t.verifyNotVisible('id=term-selector');
+      t.verifyElementNotPresent('xpath=//div[@class="wk-event-listing"]');
+    }));
+
+    it ("load a saved schedule", SodaCan(function(){
+      t.click('id=load-button');
+      t.waitForTextPresent('Open a perviously created schedule:');
+      t.click('xpath=//a[contains(.,"awesome")]');
+      t.verifyElementNotPresent('id=loadList');
+      t.verifyElementPresent('xpath=//div[@class="wk-event-listing"]');
+    }));
+
+    it ("delete schedule from load menu", SodaCan(function(){
+      t.click('id=load-button');
+      t.waitForTextPresent('Open a perviously created schedule:');
+      t.click('xpath=//div[@class="delete"]');
+      t.assertConfirmation('Delete this schedule?');
+      t.click("load_close");
+      //Todo: make this actually test the deletion?
+    }));
+
+    //Todo: test print (non-trivial)
+
+    it ("should let you share your schedule", SodaCan(function(){
+      t.click('id=link-button');
+      t.verifyTextPresent('http://gatech.courseshark.dev/');
+      t.click('share_close');
+    }));
 
   });
-
 });
--- test/controllers/users-test.js
+++ test/controllers/users-test.js
@@ -1,29 +1,98 @@
-var Browser = require("zombie")
-  , should = require("should");
-browser = new Browser();
+var soda_sync = require('soda-sync')
+  , soda = soda_sync.soda
+  , SodaCan = soda_sync.SodaCan;
 
-describe('Schedule controller', function(){
+var browser = null;
 
-  describe('Schedule page', function(){
-
-    before(function(done) {
-      this.browser = new Browser();
-      this.browser
-        .visit("http://localhost:8080/schedule", { debug: true })
-        .then(function(){
-          console.log("The page:", browser.html());
-          done()
-        })
-        .fail(function(error) {
-          console.log("Not good:", error)
-        });
-    });
-
-    it("should require you to select school if not logged in", function(done) {
-      console.log(this.browser.html());
-      done();
-    });
-
-  });
-
+SodaCan = SodaCan({
+  "with": function() {
+    return browser;
+  },
+  pre: function() {
+    return this.timeout(5000);
+  }
 });
+browser = soda.createClient({
+  host: "localhost",
+  port: 4444,
+  url: "http://courseshark.dev",
+  browser: "googlechrome",
+  mode: 'sync'
+});
+// browser.on('command', function(cmd, args){
+//   console.log(' \x1b[33m%s\x1b[0m: %s', cmd, args.join(', '));
+// });
+
+describe('User controller', function(){
+
+  describe("creating account", function(){
+    var t;
+    var unique_name = String(Date.now());
+
+    it("it lets you create an account", SodaCan(function(){
+      t = this;
+      t.session();
+      t.open('/');
+      t.waitForTextPresent('Scheduling made simple');
+      t.click('signInButton');
+      t.waitForTextPresent('Login');
+      t.click('signup');
+      t.waitForTextPresent('Register');
+      t.type('signup_email',unique_name + "@domain.com");
+      t.type('signup_password',unique_name);
+      t.type('c_password',unique_name);
+      t.select('school_id','Georgia Tech');
+      t.click('registerButton');
+      t.waitForElementPresent('accountDropdown');
+      t.testComplete();
+    }));
+
+    // Todo: Test data validation
+    // bum email, mismatched passwords, other?
+
+    it("doesn't let you create an existing account", SodaCan(function(){
+      t.session();
+      t.open('/');
+      t.waitForTextPresent('Scheduling made simple');
+      t.click('signInButton');
+      t.waitForTextPresent('Login');
+      t.click('signup');
+      t.waitForTextPresent('Register');
+      t.type('signup_email',unique_name + "@domain.com");
+      t.type('signup_password',unique_name);
+      t.type('c_password',unique_name);
+      t.select('school_id','Georgia Tech');
+      t.click('registerButton');
+      t.assertTextPresent('Email already taken');
+      t.testComplete();
+    }));
+  })
+
+  describe("logging in/out", function(){
+    var t;
+
+    it("should let you log in", SodaCan(function(){
+      t = this;
+      t.session();
+      t.open('/');
+      t.waitForTextPresent('Scheduling made simple');
+      t.click('signInButton');
+      t.waitForTextPresent('Login');
+      t.type('login_email','greg@greg.com');
+      t.type('login_password','greg');
+      t.click('loginButton');
+      t.waitForTextPresent('greg@greg.com');
+      t.assertElementPresent('accountDropdown');
+    }));
+
+    it("should let you log out", SodaCan(function(){
+      t.click('accountDropdown');
+      t.waitForTextPresent('Log Out');
+      t.click('logOut');
+      t.waitForTextPresent('Sign In');
+      t.testComplete();
+    }));
+  });
+});
+
+
