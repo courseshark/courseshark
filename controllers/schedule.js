3f802af6b13023aa56758b0e3193c255a56a8b9f
James Rundquist <james.k.rundquist@gmail.com>
2012-09-25 01:27:10 -0400

    Combined build.main + require into one. Served when !dev

--- controllers/schedule.js
+++ controllers/schedule.js
@@ -15,7 +15,7 @@ exports = module.exports = function(app){
 	})
 	
 	app.get('/s(/*)?', requireSchool, function(req, res){
-		res.render('schedule/schedule', {school: req.school._id, layout: 'app-layout.ejs', built: false});
+		res.render('schedule/schedule', {school: req.school._id, layout: 'app-layout.ejs', built: app.settings.env!="development"});
 	})
 
 
