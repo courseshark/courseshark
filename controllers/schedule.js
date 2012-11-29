9f8bf616d5941d46bbb655f41f5aa0e78d0f9c61
James Rundquist <james.k.rundquist@gmail.com>
2012-11-29 16:30:56 -0500

    Save schedule object into schema as mixed type

--- controllers/schedule.js
+++ controllers/schedule.js
@@ -89,7 +89,7 @@ exports = module.exports = function(app){
     newSchedule.school = req.school
     newSchedule.term = passedJSON.term._id
     newSchedule.save(function(err, schedule){
-      req.user.schedule = schedule;
+      req.user.schedule = newSchedule;
       req.user.save();
       if (err){
         res.send(500);
@@ -118,6 +118,9 @@ exports = module.exports = function(app){
       passedJSON.school = req.school;
       schedule.set(passedJSON);
       schedule.save(function(err){
+        req.user.schedule = schedule;
+        req.user.save();
+
         if ( err ){
           res.send(500);
         }else{
