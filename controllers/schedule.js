b21016eb74659ee1a1e09d3c4099d07564120c61
James Rundquist <james.k.rundquist@gmail.com>
2012-11-29 16:11:56 -0500

    Save primary schedule into user object on schedule save:

--- controllers/schedule.js
+++ controllers/schedule.js
@@ -89,6 +89,8 @@ exports = module.exports = function(app){
     newSchedule.school = req.school
     newSchedule.term = passedJSON.term._id
     newSchedule.save(function(err, schedule){
+      req.user.schedule = schedule;
+      req.user.save();
       if (err){
         res.send(500);
       }else{
