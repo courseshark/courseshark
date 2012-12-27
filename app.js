398c16403c4a6c1cb99e980d59e700a23f5c746e
James Rundquist <james.k.rundquist@gmail.com>
2012-12-27 00:53:13 -0500

    Fixed binding issued with socket.io

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var   express = require('express')
     , auth = require('./lib/authorization')
     , utils = require('./lib/utils')
     , app = express()
-    , port = (typeof process !== "undefined" && process !== null ? (_ref2 = process.env) !== null ? _ref2.PORT : undefined : undefined) || 80
+    , port = process.env.PORT || 8080
     , sio = require('socket.io')
     , io
     , mongoose = require('mongoose')
@@ -20,30 +20,6 @@ app.config = process.env;
 app.mixpanel = mixpanel;
 app.mixpanel.set_config({ test: (process.env.COURSESHARK_MIXPANEL_TEST||'').match(/true/i)
                         , debug: (process.env.COURSESHARK_MIXPANEL_DEBUG||'').match(/true/i)});
-
-// Socket.io
-httpForSocket = require('http').createServer(app)
-app.io = io = sio.listen(httpForSocket);
-app.io.enable('browser client minification');  // send minified client
-app.io.enable('browser client etag');          // apply etag caching logic based on version number
-app.io.enable('browser client gzip');          // gzip the file
-app.io.set('log level', 1);                    // reduce logging
-app.io.set('transports',['websocket']);        // enable all transports (optional if you want flashsocket)
-// Setup RedisStore for shared sockets
-if (process.env.COURSESHARK_REDIS_URI){
-  var info = require('url').parse(process.env.COURSESHARK_REDIS_URI)
-    , RedisStore = require('socket.io/lib/stores/redis')
-    , redis  = require('socket.io/node_modules/redis')
-    , pub    = redis.createClient(info.port, info.hostname)
-    , sub    = redis.createClient(info.port, info.hostname)
-    , client = redis.createClient(info.port, info.hostname)
-  console.log("Using Redis for socket.io store");
-  app.io.set('store', new RedisStore({
-    redisPub : pub
-  , redisSub : sub
-  , redisClient : client
-  }));
-}
 
 mongoose.connection.on('open', function(){
   console.log('Database Connected');
@@ -58,25 +34,61 @@ mongoose.connection.on('open', function(){
   require('./lib/social-track').boot(app);
   // Configuration
   require('./lib/flipflop').boot(app, mongoose, function(){
+
+
     // Configuration
     require('./lib/settings').boot(app, mongoose);
+
+
     //Error Handler
     require('./lib/error-handler').boot(app);
+
+
     // make a global helper
     requireLogin = auth.requireLogin;
     requireAdmin = auth.requireAdmin;
     requireSchool = utils.requireSchool;
     wantSchool = utils.wantSchool;
+
+
+    // Start the app by listening on <port>
+    var server = app.listen(port);
+    // Socket.io
+    app.io = io = sio.listen(server);
+    app.io.enable('browser client minification');  // send minified client
+    app.io.enable('browser client etag');          // apply etag caching logic based on version number
+    app.io.enable('browser client gzip');          // gzip the file
+    app.io.set('log level', 1);                    // reduce logging
+    app.io.set('transports',['websocket']);        // enable all transports (optional if you want flashsocket)
+    // Setup RedisStore for shared sockets
+    if (process.env.COURSESHARK_REDIS_URI){
+      var info = require('url').parse(process.env.COURSESHARK_REDIS_URI)
+        , RedisStore = require('socket.io/lib/stores/redis')
+        , redis  = require('socket.io/node_modules/redis')
+        , pub    = redis.createClient(info.port, info.hostname)
+        , sub    = redis.createClient(info.port, info.hostname)
+        , client = redis.createClient(info.port, info.hostname)
+      console.log("Using Redis for socket.io store");
+      app.io.set('store', new RedisStore({
+        redisPub : pub
+      , redisSub : sub
+      , redisClient : client
+      }));
+    }
+
+
     // Bootstrap controllers
     controller_loc = __dirname + '/controllers';
     controller_files = fs.readdirSync(controller_loc);
     controller_files.forEach( function (file) {
       require(controller_loc + '/' + file)(app);
     })
+
+
     // Build
     require('./build').build(app);
-    // Start the app by listening on <port>
-    app.listen(port);
+
+
     console.log('CourseShark started on port ' + port);
   });
 })
